cdc433512681ff830d554e59f5475e579bff4a86
JayPr <JayPr@host.docker.internal>
2021-04-18 14:17:13 -0700

    initial version

---
+++ src/main/java/com/nx/tech/engage/config/NxConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.nx.tech.engage.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class NxConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nx/tech/engage/config/NxConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nx.tech.engage.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NxConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
